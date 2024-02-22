c20f308129cfd20d98ff9e68093152a354a10541
JC <juancfoofn@yahoo.com>
2024-02-22 19:52:12 +0100

    Add full support for '%%' and '%c' and partial support for '%s'

--- ft_printf.c
+++ ft_printf.c
@@ -6,24 +6,49 @@
 /*   By: jfidalgo <jfidalgo@student.42bar(...).com  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 09:43:05 by jfidalgo          #+#    #+#             */
-/*   Updated: 2024/02/22 16:08:18 by jfidalgo         ###   ########.fr       */
+/*   Updated: 2024/02/22 19:44:09 by jfidalgo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <stdarg.h>
+#include <unistd.h>
 #include <stdlib.h>
-#include "ft_printf.h"
+
+int	write_percent(void)
+{
+	write(1, "%", 1);
+	return (1);
+}
+
+int	write_char(int ch)
+{
+	write(1, &ch, 1);
+	return (1);
+}
 
 int	ft_printf(char const *format, ...)
 {
 	va_list	vargs;
-	int		ret;
+	int		result;
 
+	result = 0;
 	if (format == NULL)
 		return (0);
 	va_start(vargs, format);
-	ret = printf(format, vargs);
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '%')
+				result += write_percent();
+			else if (*format == 'c')
+				result += write_char(va_arg(vargs, int));
+		}
+		else
+			result += write_char(*format);
+		format++;
+	}
 	va_end(vargs);
-	return (ret);
+	return (result);
 }
