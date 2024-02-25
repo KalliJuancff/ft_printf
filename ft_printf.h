4f0e1f02d5394d43e413d2f552958a27e95e3801
JC <juancfoofn@yahoo.com>
2024-02-25 21:51:25 +0100

    Update 'ft_printf.h' to include new .C files (write_char, write_string and write_number)

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: jfidalgo <jfidalgo@student.42bar(...).com  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 15:19:42 by jfidalgo          #+#    #+#             */
-/*   Updated: 2024/02/22 15:27:19 by jfidalgo         ###   ########.fr       */
+/*   Updated: 2024/02/25 21:05:16 by jfidalgo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,9 @@
 
 # include <stdarg.h>
 
+int	write_char(int ch);
+int	write_string(char *s);
+int	write_number(int n);
 int	ft_printf(char const *format, ...);
 
 #endif
